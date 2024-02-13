6153bef8d6f6380345d9568c878b703b890f8521
mori-93 <ichi.kazumori@gmail.com>
2024-02-13 15:05:03 +0900

    epands the track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,6 +2,34 @@ import React, {useCallback} from "react";
 import "./Track.css";
 
 const Track = (props) => {
+    // need to define onAdd
+    const addTrack = useCallback(
+        (event) => {
+            props.onAdd(props.track);
+        }, [props.onAdd, props.track]
+    );
+    
+    //need to define onRemove
+    const removeTrack = useCallback(
+        (event) => {
+            props.onRemove(props.track);
+        }, [props.onRemove, props.track]
+    );
+    
+    //need to define isRemoval and removeTrack/addTrack
+    const renderAction = () => {
+        if (props.isRemoval) {
+            return (
+                <button className="Track-action" onclick={removeTrack}>
+                    -
+                </button>
+            );
+        }   return (
+                <button className="Track-action" onclick={addTrack}>
+                    +
+                </button>
+        );
+    };
 
     return (
         <div className="Track">
@@ -11,7 +39,7 @@ const Track = (props) => {
                     {props.track.artist} | {props.track.album}
                 </p>
             </div>
-            {/*renderAction()*/}
+            {renderAction()}
         </div>
     );
 };

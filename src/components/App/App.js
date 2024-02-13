2e71f2536a8aea94c8b2e2a97559cdb12fc17e9d
mori-93 <ichi.kazumori@gmail.com>
2024-02-13 14:44:23 +0900

    establish the search results div including the base of each Track component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,38 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Spotify from "../../utilities/Spotify";
 
+//mockdata for searchResults:
+const mockSearchResults = [
+    {
+      id: 1,
+      name: "Yoru ni Kakeru",
+      artist: "Yoasobi",
+      album: "THE BOOK",
+    },
+    {
+      id: 2,
+      name: "Kaibutsu",
+      artist: "Yoasobi",
+      album: "THE BOOK",
+    },
+    {
+      id: 3,
+      name: "Gunjo",
+      artist: "Yoasobi",
+      album: "THE BOOK",
+    }
+  ];
+
 function App() {
+  const [searchResults, setSearchResults] = useState(mockSearchResults)
+
   return (
     <div>
       <h1>Ja<span className='highlight'>mmm</span>ing</h1>
       <div className="App">
         <p>here goes the search bar</p>
         <div className='App-playlist'>
-          <p>here goes the search results</p>
+          <SearchResults searchResults={searchResults} /*onAdd={addTrack}*//>
           <p>here goes the play list</p>
         </div>
       </div>
---
+++ src/components/Track/Track.js
@@ -0,0 +1,19 @@
+import React, {useCallback} from "react";
+import "./Track.css";
+
+const Track = (props) => {
+
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{props.track.name}</h3>
+                <p>
+                    {props.track.artist} | {props.track.album}
+                </p>
+            </div>
+            {/*renderAction()*/}
+        </div>
+    );
+};
+
+export default Track;
